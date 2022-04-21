05fa9258b2926c34b93f379f76d19eac44649045
bocbambooairways <bocbambooairways@gmail.com>
2022-04-21 09:21:08 +0700

    BOC21042022

---
+++ BOC/Areas/B-DCS/Controllers/FlightController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOC.Areas.B_DCS.Controllers
+{
+   
+    [Area("B-DCS")]
+    public class FlightController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult M_Index()
+        {
+            return View();  
+        }
+        public IActionResult M_FlightList()
+        {
+            return View();
+        }
+        public IActionResult M_SearchList()
+        {
+            return View();
+        }
+        public IActionResult M_Passenger()
+        {
+            return View();
+        }
+        public IActionResult M_PassengerList()
+        {
+            return View();
+        }
+        public IActionResult M_Comment()
+        {
+            return View();
+        }
+    }
+}
